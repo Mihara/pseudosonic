02e66586f4e96cd2f383305e51dbd6d49914fbab
Eugene Medvedev <rn3aoh.g@gmail.com>
2023-02-11 11:46:09 +0300

    Multiple profiles.

--- pseudosonic.py
+++ pseudosonic.py
@@ -29,29 +29,29 @@ parser.add_argument(
     help="Configuration file to use",
 )
 
+parser.add_argument(
+    "--profile", "-p", metavar="PROFILE", nargs="*", help="Which profile(s) to fetch."
+)
+
 args = parser.parse_args()
 
 cfg = configparser.ConfigParser()
 cfg.read_file(args.config)
 
-cfg = cfg["pseudosonic"]
+server = cfg["SERVER"]
 
 # Establish and test connection to server.
 conn = libsonic.Connection(
-    cfg["url"],
-    port=cfg.getint("port", 443)
-    if cfg["url"].startswith("https")
-    else cfg.getint("port", 80),
-    serverPath=urljoin(cfg.get("server_path", "/"), "rest"),
-    username=cfg["username"],
-    password=cfg["password"],
+    server["url"],
+    port=server.getint("port", 443)
+    if server["url"].startswith("https")
+    else server.getint("port", 80),
+    serverPath=urljoin(server.get("server_path", "/"), "rest"),
+    username=server["username"],
+    password=server["password"],
 )
 
 conn.ping()
-
-# To prevent from doing things more than once, we'll suck everything
-# into one dict by song id, where every song has an 'album' and 'artist' fields anyway.
-songlist = dict()
 
 
 def grab_song(songrec):
@@ -67,59 +67,85 @@ def grab_album(albumrec):
         grab_song(album_songs["album"]["song"])
 
 
-if not cfg.get("playlist"):
-    print("Collecting favorites.")
+def grab_profile(profile):
+    "Fetch music data per a profile"
 
-    # Now we got our library of favorites, which comes in three kinds -
-    # artist, album and song.
-    favorites = conn.getStarred2()
+    if not profile.get("playlist"):
+        print("Collecting favorites.")
 
-    # Starring an artist means I want every song they're involved with,
-    # so we recurse down.
-    for artist in favorites["starred2"]["artist"]:
-        artist_info = conn.getArtist(artist["id"])
-        grab_album(artist_info["artist"]["album"])
+        # Now we got our library of favorites, which comes in three kinds -
+        # artist, album and song.
+        favorites = conn.getStarred2()
 
-    # Now do the same for starred albums.
-    grab_album(favorites["starred2"]["album"])
+        # Starring an artist means I want every song they're involved with,
+        # so we recurse down.
+        for artist in favorites["starred2"].get("artist", []):
+            artist_info = conn.getArtist(artist["id"])
+            grab_album(artist_info["artist"].get("album", []))
 
-    # And for individual starred songs.
-    grab_song(favorites["starred2"]["song"])
+        # Now do the same for starred albums.
+        grab_album(favorites["starred2"].get("album", []))
 
-else:
-    print("Collecting playlist '{}'".format(cfg["playlist"]))
-    # Else we're dealing with a playlist, so get that.
-    playlists = conn.getPlaylists()
+        # And for individual starred songs.
+        grab_song(favorites["starred2"].get("song", []))
 
-    for playlist in playlists["playlists"]["playlist"]:
-        if playlist["name"] == cfg["playlist"]:
-            playlist_data = conn.getPlaylist(playlist["id"])
-            # Playlist entries actually appear to be songs in the end,
-            # and I assume it's the same for smart playlists...
-            grab_song(playlist_data["playlist"]["entry"])
+    else:
+        print("Collecting playlist '{}'".format(profile["playlist"]))
+        # Else we're dealing with a playlist, so get that.
+        playlists = conn.getPlaylists()
 
-# Now that we have a list, go through it, streaming every song into a file,
-# explicitly stating format and bitrate so the server does the transcoding for us.
+        for playlist in playlists["playlists"]["playlist"]:
+            if playlist["name"] == profile["playlist"]:
+                playlist_data = conn.getPlaylist(playlist["id"])
+                # Playlist entries actually appear to be songs in the end,
+                # and I assume it's the same for smart playlists...
+                grab_song(playlist_data["playlist"]["entry"])
 
-file_format = cfg.get("format", "mp3")
 
-for song in songlist.values():
-    filepath = os.path.join(
-        cfg.get("music_dir", "."),
-        FILE_LEGAL.sub("_", song["artist"]),
-        FILE_LEGAL.sub("_", song["album"]),
-    )
-    pathlib.Path(filepath).mkdir(parents=True, exist_ok=True)
-    filename = os.path.join(
-        filepath,
-        ("{:02} - ".format(song["track"]) if song["track"] else "")
-        + FILE_LEGAL.sub("_", song["title"])
-        + "."
-        + file_format,
-    )
-    filedata = conn.stream(
-        song["id"], tformat=file_format, maxBitRate=cfg.get("bitrate", 128)
-    )
-    print("Downloading {}".format(filename))
-    with open(filename, "wb") as f:
-        f.write(filedata.read())
+def get_songs(songlist, profile):
+
+    # Now that we have a list, go through it, streaming every song into a file,
+    # explicitly stating format and bitrate so the server does the transcoding for us.
+
+    file_format = profile.get("format", server.get("format", "mp3"))
+
+    for song in songlist.values():
+        filepath = os.path.join(
+            profile.get("music_dir", server.get("music_dir", ".")),
+            FILE_LEGAL.sub("_", song["artist"]),
+            FILE_LEGAL.sub("_", song["album"]),
+        )
+        pathlib.Path(filepath).mkdir(parents=True, exist_ok=True)
+        filename = os.path.join(
+            filepath,
+            ("{:02} - ".format(song["track"]) if song["track"] else "")
+            + FILE_LEGAL.sub("_", song["title"])
+            + "."
+            + file_format,
+        )
+        filedata = conn.stream(
+            song["id"],
+            tformat=file_format,
+            maxBitRate=profile.get("bitrate", server.get("bitrate", 128)),
+        )
+        print("Downloading {}".format(filename))
+        with open(filename, "wb") as f:
+            f.write(filedata.read())
+
+
+for section in cfg.sections():
+    if section == "SERVER":
+        continue
+    if len(args.profile) and section not in args.profile:
+        continue
+
+    profile = cfg[section]
+
+    print("Profile:", section)
+
+    # To prevent from doing things more than once per profile, we'll suck everything
+    # into one dict by song id, where every song has an 'album' and 'artist' fields anyway.
+    songlist = dict()
+
+    grab_profile(profile)
+    get_songs(songlist, profile)
